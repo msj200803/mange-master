d5abdb25505b0e4d73aa620983bc370901a96a7e
bsx6 <1152650793@qq.com>
2020-08-07 17:04:59 +0800

    起飞1.0

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,4 +1,5 @@
+delete from `tb_group`;
 INSERT INTO `tb_group` VALUES ('1', 'test', 'create', '2020-08-07 08:23:01', '2020-08-07 08:23:03');
 INSERT INTO `tb_group` VALUES ('2', 'dev', 'create,update', '2020-08-07 08:23:26', '2020-08-07 08:23:31');
-
+delete from `tb_user`;
 INSERT INTO `tb_user` VALUES ('1', 'GACF12', 'kai111', '123', '1', 'select', '2', '2020-08-07 08:26:45', '2020-08-07 08:26:47');
